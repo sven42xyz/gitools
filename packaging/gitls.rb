64e376215c540082a34eebe35ed8a3674f30ae4a
Sven Siepermann <sven.siepermann@tradingtwins.com>
2026-02-24 22:53:35 +0100

    added sha from the release tar ball to homebrew formula

--- packaging/gitls.rb
+++ packaging/gitls.rb
@@ -2,8 +2,7 @@ class Gitls < Formula
   desc "A fast, minimal tool to inspect and act on multiple git repositories"
   homepage "https://github.com/sven42xyz/gitools"
   url "https://github.com/sven42xyz/gitools/archive/refs/tags/v0.1.0.tar.gz"
-  # sha256 is filled in after the release tarball exists:
-  # sha256 "REPLACE_WITH_SHA256"
+  sha256 "f57cdac5055f9e4e4d86d47c835da8212e2929b20631f9944502f19bd2d8dd5c"
   license "MIT"
   head "https://github.com/sven42xyz/gitools.git", branch: "main"
 
